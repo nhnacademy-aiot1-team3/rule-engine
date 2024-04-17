ab25a7c8a888dd1c44f1b45486558711a9859fed
HyeonSeon9 <a01062704312@gmail.com>
2024-04-17 13:37:36 +0900

    first commit

---
+++ src/test/java/live/databo3/ruleengine/RuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package live.databo3.ruleengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
