a18190b83240f8a5e0c4968f9514451a42a55c51
HyeonSeon9 <a01062704312@gmail.com>
2024-05-14 10:22:46 +0900

    Fix: yml 파일 이름 변경, pom.xml 안쓰는거 삭제

--- src/main/java/live/databo3/ruleengine/RuleEngineApplication.java
+++ src/main/java/live/databo3/ruleengine/RuleEngineApplication.java
@@ -1,25 +1,16 @@
 package live.databo3.ruleengine;
 
-import live.databo3.ruleengine.ex.TestData;
-import live.databo3.ruleengine.ex.TestDataRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.util.StopWatch;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class RuleEngineApplication /*implements CommandLineRunner*/ {
-
 
 
     public static void main(String[] args) {
--- src/main/java/live/databo3/ruleengine/ex/TestDataRepository.java
+++ src/main/java/live/databo3/ruleengine/ex/TestDataRepository.java
@@ -1,31 +1,31 @@
-package live.databo3.ruleengine.ex;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
-import java.sql.PreparedStatement;
-import java.sql.Timestamp;
-import java.util.List;
-
-@Repository
-@RequiredArgsConstructor
-public class TestDataRepository {
-    private final JdbcTemplate jdbcTemplate;
-
-    @Transactional
-    public void saveAll(List<TestData> testDataList) {
-        String sql = "INSERT INTO TestData (uuid, created_At) VALUES (?, ?)";
-        jdbcTemplate.batchUpdate(
-                sql,
-                testDataList,
-                testDataList.size(),
-                (PreparedStatement ps, TestData testData) -> {
-                    ps.setString(1, testData.getUuid());
-                    ps.setTimestamp(2, Timestamp.valueOf(testData.getCreatedAt()));
-                });
-
-    }
-}
+//package live.databo3.ruleengine.ex;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.stereotype.Repository;
+//
+//import javax.transaction.Transactional;
+//import java.sql.PreparedStatement;
+//import java.sql.Timestamp;
+//import java.util.List;
+//
+//@Repository
+//@RequiredArgsConstructor
+//public class TestDataRepository {
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    @Transactional
+//    public void saveAll(List<TestData> testDataList) {
+//        String sql = "INSERT INTO TestData (uuid, created_At) VALUES (?, ?)";
+//        jdbcTemplate.batchUpdate(
+//                sql,
+//                testDataList,
+//                testDataList.size(),
+//                (PreparedStatement ps, TestData testData) -> {
+//                    ps.setString(1, testData.getUuid());
+//                    ps.setTimestamp(2, Timestamp.valueOf(testData.getCreatedAt()));
+//                });
+//
+//    }
+//}
