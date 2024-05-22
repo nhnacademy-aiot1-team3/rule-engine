e374ddba8c46c868d7140e0932bfa981c8796447
PJB <whdqlsdlfkd@gmail.com>
2024-05-22 10:40:22 +0900

    error detect logic

--- src/main/java/live/databo3/ruleengine/event/listener/ErrorDetectEventListener.java
+++ src/main/java/live/databo3/ruleengine/event/listener/ErrorDetectEventListener.java
@@ -11,40 +11,57 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ErrorDetectEventListener {
     private final ApplicationEventPublisher applicationEventPublisher;
-    private final Map<String, Object> sensorMeanValue;
+    private final Map<String, Object> sensorRefValue;
+    private final Map<String, Object> sensorRefDef;
     private final Map<String, Object> sensorErrorCount;
 
+    /*
+    각 Map 에 들어가는 데이터의 타입은 직접 제어하고 있으므로, 타입체크의 문제는 없음.
+     */
+    @SuppressWarnings("unchecked")
     @Async
-    @EventListener(condition = "#eventMessage.from instanceof T(live.databo3.ruleengine.flag.FromDataProcessing)")
-    public void dataProcessing(EventMessage<DataPayloadDto> eventMessage) {
+    @EventListener(condition = "#eventMessage.from instanceof T(live.databo3.ruleengine.flag.FromValueCheck)")
+    public void errorDetector(EventMessage<DataPayloadDto> eventMessage) {
         MessageDto<DataPayloadDto> messageDto = eventMessage.getMsg();
-
         HashMap<String, String> topicValue = TopicUtil.getTopicValue(messageDto);
         TopicDto topicDto = new ObjectMapper().convertValue(topicValue, TopicDto.class);
-
-        if (!sensorMeanValue.containsKey(topicDto.getSite())){
-            sensorMeanValue.put(topicDto.getSite(), topicDto.getDevice());
+        if (topicDto.getEndpoint().equals("temperature") || topicDto.getEndpoint().equals("co2") || topicDto.getEndpoint().equals("humidity")){
+            String targetTopic = topicDto.getDevice() + topicDto.getEndpoint();
+            if (!sensorRefValue.containsKey(targetTopic)){
+                if (!sensorRefDef.containsKey(targetTopic)){
+                    List<Double> sensorRefCalData = new ArrayList<>();
+                    sensorRefDef.put(targetTopic, sensorRefCalData);
+                }
+                if (((List<?>)sensorRefDef.get(targetTopic)).size() < 3){
+                    ((List<Double>)sensorRefDef.get(targetTopic)).add(Double.parseDouble(messageDto.getPayload().getValue().toString()));
+                }else {
+                    Double sum = 0.0;
+                    for (Double data : (List<Double>)sensorRefDef.get(targetTopic)){
+                        sum += data;
+                    }
+                    sensorRefValue.put(targetTopic, sum / 3);
+                }
+            }else {
+                if((Double) sensorRefValue.get(targetTopic) * 2 < messageDto.getPayload().getValue()){
+                    //에러 Event publish
+                    System.out.println("Errordata 발생");
+//                applicationEventPublisher.publishEvent(new EventMessage<>(this, eventMessage.getId(),));
+                }else {
+                    System.out.println("정상데이터 처리 기준값 : " + sensorRefValue.get(targetTopic) + " --- 현재값 : " + messageDto.getPayload().getValue());
+//                applicationEventPublisher.publishEvent(this);
+                }
+            }
         }
-
-        sensorMeanValue.get(topicDto.getSite());
-
-
-
-
 
 
 //        StopWatchUtil.stop(eventMessage.getId());
