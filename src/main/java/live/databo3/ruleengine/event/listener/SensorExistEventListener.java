d94aac72cdbcea643623b59aac55632365a35811
HyeonSeon9 <a01062704312@gmail.com>
2024-05-22 14:00:37 +0900

    Fix: OrganizationInfoService 수정

--- src/main/java/live/databo3/ruleengine/event/listener/SensorExistEventListener.java
+++ src/main/java/live/databo3/ruleengine/event/listener/SensorExistEventListener.java
@@ -6,16 +6,13 @@ import live.databo3.ruleengine.event.message.MessagePayload;
 import live.databo3.ruleengine.event.message.RuleEngineEvent;
 import live.databo3.ruleengine.event.message.TopicDto;
 import live.databo3.ruleengine.sensor.adaptor.SensorAdaptor;
+import live.databo3.ruleengine.service.OrganizationInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import live.databo3.ruleengine.service.OrganizationInfoService;
-
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Component
@@ -23,7 +20,6 @@ import java.util.Map;
 public class SensorExistEventListener {
     private final ApplicationEventPublisher applicationEventPublisher;
     private final OrganizationInfoService organizationInfoService;
-    private final SensorAdaptor sensorAdaptor;
 
     @Async
     @EventListener(condition = "#ruleEngineEvent.from instanceof T(live.databo3.ruleengine.flag.FromTopicSplit)")
@@ -34,7 +30,7 @@ public class SensorExistEventListener {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         } catch (FeignException.FeignClientException.BadRequest e) {
-            log.error("error meg : {} | error topic : {}",e.getMessage(),topic);
+            log.error("error meg : {} | error topic : {}", e.getMessage(), topic);
 
         }
 
--- src/main/java/live/databo3/ruleengine/service/impl/OrganizationInfoServiceImpl.java
+++ src/main/java/live/databo3/ruleengine/service/impl/OrganizationInfoServiceImpl.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -35,7 +36,7 @@ public class OrganizationInfoServiceImpl implements OrganizationInfoService {
         List<String> sensorList = redisTemplate.opsForList().range("sensorList", 0, -1).stream()
                 .map(Object::toString)
                 .collect(Collectors.toList());
-        if (!sensorList.contains(topic.getEndpoint())) {
+        if (Boolean.FALSE.equals(sensorList.contains(topic.getEndpoint()))) {
             return;
         }
 
@@ -45,18 +46,18 @@ public class OrganizationInfoServiceImpl implements OrganizationInfoService {
         if (Boolean.FALSE.equals(redisTemplate.hasKey(organizationName))) {
             sensorAdaptor.reloadRedis(organizationName);
         }
-        Map<Object, Object> entries = redisTemplate.opsForHash().entries(organizationName);
 
-        Map<String, Object> collect = objectMapper.convertValue(entries, new TypeReference<>() {
+        Map<String, Object> collect = objectMapper.convertValue(redisTemplate.opsForHash().entries(organizationName), new TypeReference<>() {
         });
 
         Object deviceInfo = collect.get("sensorList:" + deviceName);
-        log.info("{}", deviceName + " : " + topic.getEndpoint());
+        log.info("{} : {} : {}", deviceName,topic.getEndpoint(),deviceInfo);
 
 
         try {
             if (Objects.isNull(deviceInfo)) {
                 sensorAdaptor.saveSensor(topic);
+                return;
             }
             List<String> sensorTypeList = objectMapper.readValue(deviceInfo.toString(), new TypeReference<>() {
             });
@@ -66,6 +67,8 @@ public class OrganizationInfoServiceImpl implements OrganizationInfoService {
 
         } catch (FeignException.InternalServerError error) {
             log.error("{} : {} : {}", topic, deviceInfo, error.getMessage());
+        } catch (NullPointerException e) {
+            log.error("{} : {} : {} : {}","NullPoint", topic, deviceInfo, e.getMessage());
         }
     }
 }
