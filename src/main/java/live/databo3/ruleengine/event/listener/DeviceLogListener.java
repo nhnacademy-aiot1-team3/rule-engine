4226c4e752653b55bbece3bd3e3b0fb648e8f202
PJB <whdqlsdlfkd@gmail.com>
2024-05-23 18:32:12 +0900

    device control Log insert logic modified.

--- src/main/java/live/databo3/ruleengine/event/listener/DeviceLogListener.java
+++ src/main/java/live/databo3/ruleengine/event/listener/DeviceLogListener.java
@@ -2,19 +2,13 @@ package live.databo3.ruleengine.event.listener;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import live.databo3.ruleengine.event.message.*;
-import live.databo3.ruleengine.flag.FromRabbitMQ;
 import live.databo3.ruleengine.sensor.adaptor.SensorAdaptor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 @Slf4j
 @Component
